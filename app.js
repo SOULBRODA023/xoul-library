f426c54564da64b7db0a704cb05202668481a86e
Soulbroda23 <ogundeyi.dieko13@gmail.com>
2023-10-18 00:53:34 -0600

    done with the object constructor for the book

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
   let main = document.querySelector(".main");
   let body = document.querySelector("[data-body]");
-
+  let add = document.querySelector(".last-child");
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
+  const pages = document.getElementById("pages");
+  const checkbox = document.getElementById("read");
+  const modalContainer = document.getElementById("modal__container");
+  let form = document.querySelector("#form")
+  const submitButton = document.querySelector("[data-submit")
   let greeting = "";
+  const titleValue = title.value;
+  const authorValue = author.value;
+  const pageValue = pages.value;
+  const checkboxValue = checkbox.checked;
+  const myLibrary = [];
+
+
+
+
+
+
+
+
+
 
   // Create the header element
   let header = document.createElement("header");
@@ -26,17 +47,10 @@ document.addEventListener("DOMContentLoaded", function () {
     let greetingsText = document.createElement("h1");
     greetingsText.textContent = `Hello! ${greeting}, welcome to the Soul Library`;
     header.appendChild(greetingsText);
+
   };
   greetings();
-  setTimeout(() => {
-    let loadingText = document.createElement("h4")
-    let AddButton =  document.createElement("div")
-    
 
-    loadingText.textContent = `What book will you like to tell us about ${AddButton.textContent = "+"}`
-    header.appendChild(loadingText)
-    AddButton.classList.add(".add")
-  }, 1000)
 
 
 
@@ -44,4 +58,42 @@ document.addEventListener("DOMContentLoaded", function () {
   let container = document.createElement("div");
   container.classList.add("container");
   main.appendChild(container);
+  body.appendChild(modalContainer)
+  //create an object constructor 
+  function Book(title, author, pages, read) {
+    this.title = titleValue
+    this.author = authorValue
+    this.pages = pageValue
+    this.read = read
+    this.completeBook = ()=>{
+      `${title} by ${author} of ${pages} pages`
+    }
+
+  }
+
+  let ismodalOpen = false
+  add.addEventListener("click", () => {
+    ismodalOpen = !ismodalOpen
+    if (!ismodalOpen) {
+      modalContainer.style.display = "block";
+      add.textContent = "-"
+    }
+    else if (ismodalOpen) {
+      modalContainer.style.display = "none";
+      add.textContent = "+"
+    }
+
+  })
+
+  const kathryn = new Book('anointing', 'Kathryn Khulman', '34')
+
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    titleValue.value = "";
+    pageValue.value = "";
+    authorValue.value = "";
+    console.log(kathryn.completeBook)
+  })
+
 });
